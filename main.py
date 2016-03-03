ab4d65f42fb33db403db2fd99afb75b3424918d0
Shun Liang <shun.liang.public@gmail.com>
2016-03-03 21:23:50 +0000

    Added get and set end points to server.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+set_request = ''
 
 @app.route("/")
 def hello():
     return "[[255, 255, 255], [0, 255, 0], [0, 0, 0], [255, 0, 0], [0, 0, 255], [0, 255, 255], [255, 0, 255], [255, 255, 0]]"
 
+@app.route("/set", methods=['POST'])
+def set_lights():
+    global set_request
+    if request.method == 'POST':
+        set_request = request.data
+    return ''
+
+@app.route("/get")
+def get_lights():
+    print set_request
+    return set_request
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
