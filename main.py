04a04d0be707e95db76cc771d2a350b140e51e00
Shun Liang <shun.liang.public@gmail.com>
2016-03-03 20:52:31 +0000

    Changed reponse to be dummy json array.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Python Dojo!"
+    return "[[255, 255, 255], [0, 255, 0], [0, 0, 0], [255, 0, 0], [0, 0, 255], [0, 255, 255], [255, 0, 255], [255, 255, 0]]"
 
 if __name__ == "__main__":
     app.run()
